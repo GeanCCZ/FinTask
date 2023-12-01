fc99a3a3fc7efec9a84a944398572f9ad51f0efe
Gean Carlos Costa de Souza <ra-20015442-2@alunos.unicesumar.edu.br>
2023-12-01 09:53:52 -0300

    antigo

--- productive-plan-front/src/app/side-bar.tsx
+++ productive-plan-front/src/app/side-bar.tsx
@@ -2,8 +2,8 @@ import { Children } from "react";
 
 export default function SideBar({children }: {children: React.ReactNode}){
     return(
-    <div className="bg-red-500 w-full h-96 max-h-full m-0">
-        <div className="bg-black ml-5 mt-11 mb-11 w-11 h-full rounded-md content-center gap-5 justify-center flex flex-row flex-wrap ">
+    <div className="bg-red-500 w-full h-96 max-h-full">
+        <div className="bg-black ml-5  mb-11 w-11 h-full rounded-md content-center gap-5 justify-center flex flex-row flex-wrap ">
 
             <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-9 h-9">
                 <path strokeLinecap="round" strokeLinejoin="round" d="M6.75 3v2.25M17.25 3v2.25M3 18.75V7.5a2.25 2.25 0 012.25-2.25h13.5A2.25 2.25 0 0121 7.5v11.25m-18 0A2.25 2.25 0 005.25 21h13.5A2.25 2.25 0 0021 18.75m-18 0v-7.5A2.25 2.25 0 015.25 9h13.5A2.25 2.25 0 0121 11.25v7.5m-9-6h.008v.008H12v-.008zM12 15h.008v.008H12V15zm0 2.25h.008v.008H12v-.008zM9.75 15h.008v.008H9.75V15zm0 2.25h.008v.008H9.75v-.008zM7.5 15h.008v.008H7.5V15zm0 2.25h.008v.008H7.5v-.008zm6.75-4.5h.008v.008h-.008v-.008zm0 2.25h.008v.008h-.008V15zm0 2.25h.008v.008h-.008v-.008zm2.25-4.5h.008v.008H16.5v-.008zm0 2.25h.008v.008H16.5V15z" />
---
+++ productive-plan-front/src/app/wallet/page.tsx
@@ -0,0 +1,7 @@
+export default function wallet(){
+    return(
+        <div className="bg-blue-500">
+            aaaweeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeed
+        </div>
+    )
+}
